688a00d144af934c00ef029a9e77cce4ee6bb413
mfonido mark <favourmark05@gmail.com>
2021-02-12 04:41:32 +0100

    implemented art and craft chat room

--- src/components/Chats/craft.vue
+++ src/components/Chats/craft.vue
@@ -1,57 +1,78 @@
 <template>
-    <div>
-        <p class="text-white text-center py-1">Craft Chat</p>
+    <div class="GeneralChat">
+      <p class="text-white text-center py-1">Art and Craft</p>
         <div class="mesgs">
           <div class="msg_history">
-            <div class="incoming_msg">
-              <!-- <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div> -->
-              <div class="received_msg">
+            <div v-for="(message, index) in messages" :key="index" class="incoming_msg">
+              <div :class="[message.author === profile.fullName?'sent_msg':'received_msg']">
                 <div class="received_withd_msg">
-                  <p>Test which is a new approach to have all
-                    solutions</p>
-                  <span class="time_date"> 11:01 AM    |    June 9</span></div>
-              </div>
-            </div>
-            <div class="outgoing_msg">
-              <div class="sent_msg">
-                <p>Test which is a new approach to have all
-                  solutions</p>
-                <span class="time_date"> 11:01 AM    |    June 9</span> </div>
-            </div>
-            <div class="incoming_msg">
-              <!-- <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div> -->
-              <div class="received_msg">
-                <div class="received_withd_msg">
-                  <p>Test, which is a new approach to have</p>
-                  <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
-              </div>
-            </div>
-            <div class="outgoing_msg">
-              <div class="sent_msg">
-                <p>Apollo University, Delhi, India Test</p>
-                <span class="time_date"> 11:01 AM    |    Today</span> </div>
-            </div>
-            <div class="incoming_msg">
-              <!-- <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div> -->
-              <div class="received_msg">
-                <div class="received_withd_msg">
-                  <p>We work directly with our designers and suppliers,
-                    and sell direct to you, which means quality, exclusive
-                    products, at a price anyone can afford..</p>
-                  <span class="time_date"> 11:01 AM    |    Today</span></div>
+                  <p> {{ message.message }} </p>
+                  <span class="time_date">   {{ message.createdAt | checkDate }} {{ message.author }} </span></div>
               </div>
             </div>
           </div>
           <div class="type_msg">
             <div class="input_msg_write">
-              <input type="text" class="write_msg" placeholder="Type a message" />
-              <button class="msg_send_btn" type="button"><i class="fas fa-paper-plane"></i></button>
+              <input @keyup.enter="sendMessage" v-model="message" type="text" class="write_msg" placeholder="Type a message" />
+              <button  @click="sendMessage" class="msg_send_btn" type="button"><i class="fas fa-paper-plane"></i></button>
             </div>
           </div>
         </div>
     </div>
 </template>
 
+<script>
+import { db } from '../../firebase'
+export default {
+  name: 'craft',
+  data () {
+    return {
+      message: null,
+      messages: [],
+      profile: []
+    }
+  },
+  firestore (e) {
+    if (this.auth.currentUser) {
+      const user = this.auth.currentUser
+      return {
+        profile: db.collection('profiles').doc(user.uid)
+      }
+    }
+  },
+  methods: {
+    scrollToBottom () {
+      var box = document.querySelector('.msg_history')
+      box.scrollTop = box.scrollHeight
+    },
+    sendMessage () {
+      db.collection('Art_and_craft').add({
+        message: this.message,
+        createdAt: new Date().toLocaleString(),
+        author: this.profile.fullName
+      }).then(() => {
+        this.scrollToBottom()
+      })
+      this.message = null
+    },
+    fetchMessages () {
+      db.collection('Art_and_craft').orderBy('createdAt').onSnapshot((querySnapshot) => {
+        const allMessages = []
+        querySnapshot.forEach(doc => {
+          allMessages.push(doc.data())
+        })
+        this.messages = allMessages
+        setTimeout(() => {
+          this.scrollToBottom()
+        }, 1000)
+      })
+    }
+  },
+  created () {
+    this.fetchMessages()
+  }
+}
+</script>
 <style scoped>
 .container{max-width:1170px; margin:auto;}
 img{ max-width:100%;}
@@ -154,11 +175,13 @@ img{ max-width:100%;}
   margin: 0; color:#fff;
   padding: 5px 10px 5px 12px;
   width:100%;
+  display: inline-block !important;
 }
 .outgoing_msg{ overflow:hidden; margin:26px 0 26px;}
 .sent_msg {
-  float: right;
+  float:right;
   width: 46%;
+  display: block !important;
 }
 .input_msg_write input {
   background: rgba(0, 0, 0, 0) none repeat scroll 0 0;
