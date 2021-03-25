4e39e01fb1af1e3be579a2e64f8165a75682798e
mfonido mark <favourmark05@gmail.com>
2021-03-25 23:38:59 +0100

    404 page created

---
+++ src/components/404Page.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+            <h1>404</h1>
+        </div>
+        <div class="col-sm">
+            <h2 class="text-light">
+                We couldn't find this page
+            </h2>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
